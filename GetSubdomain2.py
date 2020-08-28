2ced5aee8df0bfbcb426decafcf2e3c004080396
Michael-Scofields <z977780633@163.com>
2020-08-28 14:50:51 +0800

    init

---
+++ GetSubdomain2.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import urllib2
+import json
+import sys
+import os
+
+'''
+description:基于百度云观测接口获取子域名(Python2)
+Src:http://ce.baidu.com/index/getRelatedSites?site_address=xxx.xxx
+'''
+
+
+def GetSubdomain(domain):
+    url = 'http://ce.baidu.com/index/getRelatedSites?site_address=%s' % domain
+    req = urllib2.Request(url)
+    res = urllib2.urlopen(req)
+    res = res.read()
+    return res
+
+
+def DownSubdomain(domain_json, outfile):
+    with open(outfile, 'w') as f:
+        obj_json = json.loads(domain_json)
+        domain_list = obj_json.get("data")
+        print ('[+] Number of subdomains: {}'.format(len(domain_list)))
+        for domain in domain_list:
+            f.write(domain.get('domain'))
+            f.write('\n')
+        print ("[+] Successfully exported to {}".format(outfile))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print ('Usag: python GetSubdomain.py xxx.com')
+        exit()
+
+    target_domain = sys.argv[1]
+    outfile = "{}.txt".format(sys.argv[1])
+    outfile = "{}.txt".format(sys.argv[1])
+    if os.path.exists(outfile):
+        os.remove(outfile)
+    domain_json = GetSubdomain(target_domain)
+    DownSubdomain(domain_json, outfile)
